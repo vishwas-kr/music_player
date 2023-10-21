fc587d2f7186faa17203d4a81d52978af16ceb57
Vishwas <vishwaskumar445@gmail.com>
2023-10-21 15:23:31 +0530

    base files created

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,39 @@
+/// The [AppConstants] class is a collection of static constants that can be used
+/// throughout the application for a consistent look and feel.
+///
+class AppConstants {
+  /// The default borderRadius
+  static const double borderRadius = 8.0;
+
+  /// The default unit of spacing
+  static const double spaceUnit = 16;
+
+  /// sm spacing value (8pt)
+  static const double sm = 0.5 * spaceUnit;
+
+  /// md spacing value (12pt)
+  static const double md = 0.75 * spaceUnit;
+
+  /// lg spacing value (16pt)
+  static const double lg = spaceUnit;
+
+  /// xlg spacing value (24pt)
+  static const double xlg = 1.5 * spaceUnit;
+
+  static const appName = 'Breaking News';
+
+  static const text12 =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut condimentum risus';
+
+  static const text24 =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque lacus accumsan dolor commodo maximus. Ut purus libero, dictum vel justo id, congue malesuada.';
+
+  static const text36 =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque sed quam eu nibh posuere scelerisque non at ligula. Cras blandit.';
+
+  static const text48 =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id condimentum ex. Fusce leo lacus, convallis eu orci et, eleifend tempus libero. Morbi egestas ligula fermentum massa interdum auctor. Phasellus pretium ac nulla quis tempus. Fusce vulputate, erat non luctus scelerisque, nisl risus mollis elit, eget maximus magna.';
+
+  static const text96 =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut odio lobortis, maximus velit quis, tempor neque. Curabitur ullamcorper turpis vitae urna bibendum ullamcorper. Vestibulum sit amet bibendum sem, at ornare erat. Nunc fringilla, lectus non lobortis sagittis, eros elit lacinia ligula, id ullamcorper dolor justo a massa. Nulla vel enim vestibulum, tincidunt erat consequat, mollis sem. Fusce at augue et turpis commodo posuere eu ac libero. Integer aliquam, lectus sit amet pulvinar sagittis, neque quam egestas velit, ut dapibus purus nisi ut lorem. Integer sollicitudin convallis neque quis molestie. Sed id ultricies elit. In sagittis.';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'features/home/pages/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,9 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: const HomeScreen(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
